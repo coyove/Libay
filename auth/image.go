e7701b3d5635e0652db2925f3f5f3032080be70c
coyove <coyove@hotmail.com>
2016-12-15 21:34:08 -0500

    Image rotation functions

--- auth/image.go
+++ auth/image.go
@@ -81,3 +81,68 @@ func ResizeImage(imageBuf []byte, fn string, nwidth, nheight int, quality riLeve
 
 	return nil
 }
+
+func RotateImage(imageBuf []byte, fn string, clockwise bool, quality riLevel) error {
+	ext := strings.ToLower(filepath.Ext(fn))
+	if ext == ".gif" {
+		return ioutil.WriteFile(fn, imageBuf, 0777)
+	}
+
+	img, _, err := image.Decode(bytes.NewReader(imageBuf))
+	if err != nil {
+		return err
+	}
+
+	rect := img.Bounds()
+	width := rect.Size().X
+	height := rect.Size().Y
+
+	final := image.NewRGBA(image.Rect(0, 0, height, width))
+	rgba := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.Draw(rgba, rect, img, rect.Min, draw.Src)
+
+	if clockwise {
+		for x := 0; x < width; x++ {
+			for y := 0; y < height; y++ {
+				idx := ((height - 1 - y) + x*height) << 2
+				idx2 := (y*width + x) << 2
+
+				final.Pix[idx] = rgba.Pix[idx2]
+				final.Pix[idx+1] = rgba.Pix[idx2+1]
+				final.Pix[idx+2] = rgba.Pix[idx2+2]
+				final.Pix[idx+3] = rgba.Pix[idx2+3]
+			}
+		}
+	} else {
+		for x := 0; x < width; x++ {
+			for y := 0; y < height; y++ {
+				idx := ((width-x-1)*height + y) << 2
+				idx2 := (y*width + x) << 2
+
+				final.Pix[idx] = rgba.Pix[idx2]
+				final.Pix[idx+1] = rgba.Pix[idx2+1]
+				final.Pix[idx+2] = rgba.Pix[idx2+2]
+				final.Pix[idx+3] = rgba.Pix[idx2+3]
+			}
+		}
+	}
+
+	f, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	switch ext {
+	case ".jpg", ".jpeg":
+		jpeg.Encode(f, final, &jpeg.Options{Quality: quality[0]})
+	case ".png":
+		enc := png.Encoder{}
+		enc.CompressionLevel = png.CompressionLevel(quality[1])
+		enc.Encode(f, final)
+	default:
+		return errors.New("Format not supported")
+	}
+
+	return nil
+}
